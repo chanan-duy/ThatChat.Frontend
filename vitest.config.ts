fc53a2799b8adc1ab97125e7e278b3af61d0eec4
chanan-duy <chanan.duy@proton.me>
2025-12-10 02:33:45 +0500

    Use `allure-vitest`

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,6 +9,17 @@ export default mergeConfig(
 			environment: 'jsdom',
 			exclude: [...configDefaults.exclude, 'e2e/**'],
 			root: fileURLToPath(new URL('./', import.meta.url)),
+
+			setupFiles: ['allure-vitest/setup'],
+			reporters: [
+				'verbose',
+				[
+					'allure-vitest/reporter',
+					{
+						resultsDir: 'allure-results',
+					},
+				],
+			],
 		},
 	}),
 )
